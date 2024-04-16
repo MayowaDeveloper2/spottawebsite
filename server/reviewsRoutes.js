1067dd4bed7a866ebd702a0179d47644043cf3df
MayowaDeveloper2 <akintoyemayowa1@gmail.com>
2024-04-16 16:02:38 +0100

    all task

--- server/reviewsRoutes.js
+++ server/reviewsRoutes.js
@@ -1,18 +1,30 @@
 const express = require('express');
+const { Pool } = require('pg');
 const router = express.Router();
 const cors = require('cors');
-const Review = require('./models/Review');
 
-router.use(cors({
-    origin: "https://spottawebsite-api.vercel.app",
-    methods: ["POST", "GET"],
-    credentials: true
-}));
+const pool = new Pool({
+    user: 'default',
+    host: 'https://ep-patient-dream-a4ircu4i-pooler.us-east-1.aws.neon.tech',
+    database: 'verceldb',
+    password: 'Ur92WuQwMpZa',
+    port: 5432,
+}); 
+
+
+router.use(cors(
+    {
+        origin: "https://spottawebsite-api.vercel.app",
+        methods: ["POST", "GET"],
+        credentials: true
+    }
+));
 
 async function getReviews(req, res) {
     try {
-        const reviews = await Review.find();
-        res.status(200).json(reviews);
+        const query = 'SELECT * FROM review;';
+        const { rows } = await pool.query(query);
+        res.status(200).json(rows);
     } catch (err) {
         console.error(err);
         res.status(500).send('Failed to fetch reviews');
